410150df718a81bb8e4bbbdce9901c5160aedd94
Esteban Garcia <egarcia@teco.com.ar>
2026-04-02 17:10:55 -0300

    Solución v2: Ajuste de archivos de configuración para compatibilidad con Expo Snack y agregado de index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+import { registerRootComponent } from 'expo';
+import App from './App';
+
+// registerRootComponent hace que se invoque a App.tsx como el componente raíz.
+registerRootComponent(App);
